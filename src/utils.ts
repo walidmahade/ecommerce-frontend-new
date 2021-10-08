49e71ca75937ac7851cbe449e1df982e67c139e6
walidmahade <walidmahade@gmail.com>
2021-10-08 16:17:32 +0600

    update utils file

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,1 @@
-// export const getSiblings = (element: any) => {
-//   let siblings = [];
-//   let sibling = element.parentNode.firstElementChild;
-
-//   while(sibling) {
-//       if (sibling.nodeType === 1 && sibling !== element) {
-//           siblings.push(sibling);
-//           sibling = sibling.nextElementSibling;
-//       }
-//   }
-
-//   return siblings;
-// }
+export {}
